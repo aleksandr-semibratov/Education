b585e5dcc25c1d80a3148e5d3932abb0ec415561
Alexander Semibratov <aleksandr.semibratov@resolventagroup.com>
2022-07-27 00:29:34 +0400

    EDU-14 Add routes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import PageHome from "@/components/PageHome";
+import PageAbout from "@/components/PageAbout";
+import PageNews from "@/components/PageNews";
+import PageNotFound from "@/components/PageNotFound";
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: PageHome },
+    { path: '/news', component: PageNews },
+    { path: '/about', component: PageAbout },
+    { path: '/404', component: PageNotFound },
+    { path: '/:catchAll(.*)', redirect: '/404' }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
