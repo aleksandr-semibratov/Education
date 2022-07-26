541a4973a8f25a53fb4a090451c1208576fd1367
Alexander Semibratov <aleksandr.semibratov@resolventagroup.com>
2022-07-27 00:32:00 +0400

    EDU-14 Change template App. Add header menu and route view

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h3>Menu</h3>
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/news">News</router-link></li>
+      <li><router-link to="/another-page">Another page</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderMenu"
+}
+</script>
